149285d1ff40619019a2637f998de01a1e2f0d1b
sebastian alvarez <alvarezpaezsebastian03@gmail.com>
2023-06-10 14:04:44 +0200

    finalizado

--- main.js
+++ main.js
@@ -4,14 +4,14 @@ async function CountrysAPI() {
     console.log(countrys);
     
 const secction = document.querySelector("section")
-countrys.forEach(e => {
+countrys.forEach((e) => {
     secction.innerHTML += `<div class="country">
     <h2>${e.name.common}</h2>
     <img src="${e.flags.png}" alt="${e.flags.alt}" width="320px" height="160px">
     <p>poblacion =${e.population}</p>
     <p>region= ${e.region}</p>
     <p>capital = ${e.capital}</p>
-    <button class="moreInfo"><a href="./index2.html">more info</a></button> `
+    <button class="moreInfo"><a href="./index2.html?name=${e.name.common}">more info</a></button> `
     
 });
 
