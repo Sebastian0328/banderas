f0e24fa436128b2f3490f3ed7599d7fcf1c42f03
sebastian alvarez <alvarezpaezsebastian03@gmail.com>
2023-06-14 18:30:24 +0200

    apidelclima

--- moreInfo/info.js
+++ moreInfo/info.js
@@ -1,13 +1,14 @@
 const querySelector = window.location.search;
-const urlparams= new URLSearchParams(querySelector);
+const urlparams = new URLSearchParams(querySelector);
 const country = urlparams.get('name')
 async function getData() {
     const response = await fetch(`https://restcountries.com/v3.1/name/${country}`)
     const data = await response.json()
-console.log(data);    
-const section = document.querySelector('body')
-data.forEach(e => {
-    section.innerHTML=`<div class="country">
+
+    console.log(data);
+    const section = document.querySelector('body')
+    data.forEach(e => {
+        section.innerHTML = `<div class="country">
     <h2>${e.name.common}</h2>
     <img src="${e.flags.png}" alt="${e.flags.alt}" width="320px" height="160px">
     <p>Poblacion =${e.population}</p>
@@ -15,8 +16,9 @@ data.forEach(e => {
     <p>Capital = ${e.capital}</p>
     <p>Idiomas =${Object.values(e.languages)}</p>
     <p>Subregion =${e.subregion} </p>
-    <p>Monedas =${(Object.values(e.currencies))[0].name}</p>`
-});
+    <p>Monedas =${(Object.values(e.currencies))[0].name}</p>
+    `
+    });
 
 }
 getData()
